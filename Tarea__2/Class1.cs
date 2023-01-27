0429df284d957cfe28a65a71e94e42a0c9a672fa
rdavidmza95 <101686778+rdavidmza95@users.noreply.github.com>
2023-01-26 20:21:23 -0600

    Commit inicial

---
+++ Tarea__2/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea__2
+{
+    public class Expresion1
+    {
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+
+
+        public void pedir_digitos()
+        {
+            Console.WriteLine("Digite el primer numero");
+            Num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite el segundo numero");
+            Num2 = double.Parse(Console.ReadLine());
+
+        }
+
+        public void Sumar()
+        {
+            Console.WriteLine("La suma es: ");
+            Console.WriteLine(Num1 + Num2);
+
+        }
+
+        public void dividir()
+        {
+            Console.WriteLine("La division es: ");
+            Console.WriteLine(Num1 / Num2);
+
+        }
+
+        public void multiplicar()
+        {
+
+            Console.WriteLine("La Multiplicacion es: ");
+            Console.WriteLine(Num1 * Num2);
+
+
+        }
+
+        public void restar()
+        {
+            Console.WriteLine("La resta es:");
+            Console.WriteLine(Num1 - Num2);
+
+        }
+
+        public void Maximo()
+        {
+            if (Num1 > Num2)
+            {
+                Console.WriteLine("El maximo es: ");
+                Console.WriteLine(Num1);
+
+            }
+            else
+            {
+                Console.WriteLine("El maximo es: ");
+                Console.WriteLine(Num2);
+
+            }
+
+        }
+
+        public void Minimo()
+        {
+            if (Num1 < Num2)
+            {
+                Console.WriteLine("El minimo es: ");
+                Console.WriteLine(Num1);
+
+            }
+            else
+            {
+                Console.WriteLine("El minimo es: ");
+
+                Console.WriteLine(Num2);
+
+            }
+            Console.ReadLine();
+        }
+
+        public void resultado()
+        {
+            Sumar();
+            restar();
+            multiplicar();
+            dividir();
+            Maximo();
+            Minimo();
+        }
+
+    }
+}
---
+++ Tarea__2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea__2;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+
+        Expresion1 ex = new Expresion1();
+
+        ex.pedir_digitos();
+        ex.Sumar();
+        ex.dividir();
+        ex.multiplicar();
+        ex.restar();
+        ex.Maximo();
+        ex.Minimo();
+
+        Expresion1 ex2 = new Expresion1();
+        ex2.pedir_digitos();
+        ex2.Sumar();
+        ex2.dividir();
+        ex2.multiplicar();
+        ex2.restar();
+        ex2.Maximo();
+        ex2.Minimo();
+
+    }
+}
